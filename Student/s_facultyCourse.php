11f9c43398be1db2e4aa45b9b3bb5894bec79296
Caleb <greenfcg@gmailcom>
2022-11-28 16:12:14 -0800

    faculty course changes

--- Student/s_facultyCourse.php
+++ Student/s_facultyCourse.php
@@ -51,7 +51,7 @@ if(!isset($_SESSION['userID'])){
                     INNER JOIN Users U on T.teacherID = U.userID";
         $result = mysqli_query($connection, $select);
         echo "<form action='s_facultyCourse.php' method='post'>
-                    <label for = 'facultySelect'> Search Teacher:</label>
+                    <label for = 'facultySelect'> Search Instructor:</label>
                     <input type = 'text' name = 'facultySelect' id = 'facultySelect' list = 'facultyList'>
                     <datalist id = 'facultyList'>
                     <option value = 'All Faculty'>All Faculty</option>";
@@ -109,7 +109,7 @@ if(!isset($_SESSION['userID'])){
                 $result = mysqli_query($connection, $select);
                 $row = mysqli_fetch_array($result);
                 if($result){
-                    echo "<h3>Teacher Info</h3>
+                    echo "<h3>Instructor Info</h3>
                             <ul>
                                 <li><strong>Name:</strong> $row[1] $row[2] $row[3]</li>
                                 <li><strong>ID:</strong> $row[0]</li>
@@ -151,23 +151,238 @@ if(!isset($_SESSION['userID'])){
 
             // All teachers one course - - - - - - - - - - - - - - - - - - - - - - - - - 
             }elseif($courseSelection != "All Courses" and $facultyID == "All Faculty"){
+                $select = "SELECT C.title, C.courseNumber, M.title
+                            FROM Courses C 
+                            INNER JOIN Majors M on C.majorID = M.id
+                            WHERE C.id = $courseSelection";
+                $result = mysqli_query($connection, $select);
+                $row = mysqli_fetch_array($result);
+                if($result){
+                    echo "<h3>Course Info</h3>
+                            <ul>
+                                <li><strong>Number:</strong> $row[1]</li>
+                                <li><strong>Title:</strong> $row[0]</li>
+                                <li><strong>Major:</strong> $row[2]</li>
+                            </ul>";
 
-                echo "<h4>All teachers - One course</h4>";
+                    echo "<h3>Prerequisites</h3>";
 
+                    $selectPR = "SELECT PR.preReqID
+                            FROM courseprerequisites PR 
+                            INNER JOIN Courses C on PR.courseID = C.id
+                            WHERE C.id = $courseSelection";
+
+                    $resultPR = mysqli_query($connection, $selectPR);
+                    $count = mysqli_num_rows($resultPR);
+                    if($resultPR and $count > 0){
+                        while($rowPR = mysqli_fetch_array($resultPR)){
+
+                            $selectPName = "SELECT title, courseNumber
+                                FROM Courses 
+                                WHERE id = $rowPR[0]";
+                            $resultPName = mysqli_query($connection, $selectPName);
+                            $rowName = mysqli_fetch_array($resultPName);
+                            echo "<ul>
+                                    <li>$rowName[1]: $rowName[0]</li>
+                                </ul>";
+                            
+                        }
+                    }else{
+                        echo "<ul>
+                                <li><em>N/A</em></li>
+                            </ul>";
+                    }
+
+
+                    echo "<h3>Instructors: $semester</h3>";
+                    $selectC = "SELECT T.title, U.firstName, U.lastName, T.officePhone, T.officeNumber, CS.dateTime, CS.location, CS.totalNumSeats
+                                FROM courseSchedule CS
+                                INNER JOIN Teachers T on T.teacherID = CS.teacherID
+                                INNER JOIN Users U on U.userID = T.teacherID
+                                WHERE CS.courseID = $courseSelection
+                                AND CS.semester = '$semester'";
+                    $resultC = mysqli_query($connection, $selectC);
+                    $count = mysqli_num_rows($resultC);
+                    if($resultC and $count > 0){
+                        echo "<table>
+                                <tr>
+                                    <th><strong>Professor</strong></th>
+                                    <th><strong>Office Location</strong></th>
+                                    <th><strong>Office Phone</strong></th>
+                                    <th><strong>Course Date/Time</strong></th>
+                                    <th><strong>Course Location</strong></th>
+                                    <th><strong>Total Seats</strong></th>
+                                </tr>";
+                        while($rowC = mysqli_fetch_array($resultC)){
+                            echo "<tr>
+                                    <th>$rowC[0] $rowC[1] $rowC[2]</th>
+                                    <th>$rowC[4]</th>
+                                    <th>$rowC[3]</th>
+                                    <th>$rowC[5]</th>
+                                    <th>$rowC[6]</th>
+                                    <th>$rowC[7]</th>
+                                </tr>";
+                        }
+                        echo "</table>";
+                    }else{
+                        echo "<ul>
+                                <li><em>$row[1] does not have an assigned instructor for $semester.</em></li>
+                            </ul>";
+                    }
+                }
 
 
             // All teachers all courses - - - - - - - - - - - - - - - - - - - - - - - - - 
             }elseif($courseSelection == "All Courses" and $facultyID == "All Faculty"){
+                
+                echo "<h3>All Courses and Instructors: $semester</h3>";
 
-                echo "<h4>All teachers - All courses</h4>";
-
+                $select = "SELECT C.id
+                            FROM Courses C";
+                $result = mysqli_query($connection, $select);
+                while($row = mysqli_fetch_array($result)){
+                    $selectI = "SELECT C.title, C.courseNumber, M.title
+                            FROM Courses C 
+                            INNER JOIN Majors M on C.majorID = M.id
+                            WHERE C.id = $row[0]";
+                    $resultI = mysqli_query($connection, $selectI);
+                    $rowI = mysqli_fetch_array($resultI);
+                    if($resultI){
+                        echo "<h4>$rowI[1]: $rowI[0]  -  $rowI[2]</h4>";
+                    
+                        $selectC = "SELECT T.title, U.firstName, U.lastName, T.officePhone, T.officeNumber, CS.dateTime, CS.location, CS.totalNumSeats
+                                    FROM courseSchedule CS
+                                    INNER JOIN Teachers T on T.teacherID = CS.teacherID
+                                    INNER JOIN Users U on U.userID = T.teacherID
+                                    WHERE CS.courseID = $row[0]
+                                    AND CS.semester = '$semester'";
+                        $resultC = mysqli_query($connection, $selectC);
+                        $count = mysqli_num_rows($resultC);
+                        if($resultC and $count > 0){
+                            echo "<table>
+                                    <tr>
+                                        <th><strong>Professor</strong></th>
+                                        <th><strong>Office Location</strong></th>
+                                        <th><strong>Office Phone</strong></th>
+                                        <th><strong>Course Date/Time</strong></th>
+                                        <th><strong>Course Location</strong></th>
+                                        <th><strong>Total Seats</strong></th>
+                                    </tr>";
+                            while($rowC = mysqli_fetch_array($resultC)){
+                                echo "<tr>
+                                        <th>$rowC[0] $rowC[1] $rowC[2]</th>
+                                        <th>$rowC[4]</th>
+                                        <th>$rowC[3]</th>
+                                        <th>$rowC[5]</th>
+                                        <th>$rowC[6]</th>
+                                        <th>$rowC[7]</th>
+                                    </tr>";
+                            }
+                            echo "</table>";
+                        }
+                    }
+                    echo "<hr>";
+                }
 
 
             // one teacher one course - - - - - - - - - - - - - - - - - - - - - - - - - 
             }else{
-                
-                echo "<h4>One teacher - One course</h4>";
+                $select = "SELECT T.teacherID, T.title, U.firstName, U.lastName, T.officeNumber, T.officePhone, D.title 
+                            FROM Teachers T 
+                            INNER JOIN Users U on T.teacherID = U.userID
+                            INNER JOIN Departments D on T.departmentID = D.id
+                            WHERE T.teacherID = $facultyID";
+                $result = mysqli_query($connection, $select);
+                $row = mysqli_fetch_array($result);
+                if($result){
+                    echo "<h3>Instructor Info: $semester</h3>
+                            <ul>
+                                <li><strong>Name:</strong> $row[1] $row[2] $row[3]</li>
+                                <li><strong>ID:</strong> $row[0]</li>
+                                <li><strong>Office Location:</strong> $row[4]</li>
+                                <li><strong>Office Phone:</strong> $row[5]</li> 
+                                <li><strong>Department:</strong> $row[6]</li>
+                            </ul>";
 
+                    $selectC = "SELECT C.courseNumber, C.title, CS.dateTime, CS.location, CS.totalNumSeats
+                        FROM courseSchedule CS
+                        INNER JOIN Teachers T on T.teacherID = CS.teacherID
+                        INNER JOIN Courses C on C.id = CS.courseID
+                        WHERE CS.teacherID = $facultyID
+                        AND CS.semester = '$semester'
+                        AND CS.courseID = $courseSelection";
+                    $resultC = mysqli_query($connection, $selectC);
+                    $count = mysqli_num_rows($resultC);
+                    $rowC = mysqli_fetch_array($resultC);
+
+                    if($count == 0){
+                        echo "<ul>
+                                <li><em>$row[1] $row[2] $row[3] will not teach the selected course in $semester.</em></li>
+                            </ul>";
+                       
+                    }else{
+                        echo "<h3>Course Info: $semester</h3>
+                                <ul>
+                                    <li><strong>Number:</strong> $rowC[1]</li>
+                                    <li><strong>Title:</strong> $rowC[0]</li>
+                                    <li><strong>Major:</strong> $rowC[2]</li>
+                                </ul>";
+                        
+                        echo "<table>
+                                <tr>
+                                    <th><strong>Course Date/Time</strong></th>
+                                    <th><strong>Course Location</strong></th>
+                                    <th><strong>Total Seats</strong></th>
+                                </tr>";
+
+                        echo "<tr>
+                                <th>$rowC[2]</th>
+                                <th>$rowC[3]</th>
+                                <th>$rowC[4]</th>
+                            </tr>";
+
+                        while($rowC = mysqli_fetch_array($resultC)){
+                            echo "<tr>
+                                    <th>$rowC[2]</th>
+                                    <th>$rowC[3]</th>
+                                    <th>$rowC[4]</th>
+                                </tr>";
+                        }
+                        echo "</table>";
+
+                        echo "<h3>Prerequisites</h3>";
+
+                        $selectPR = "SELECT PR.preReqID
+                                FROM courseprerequisites PR 
+                                INNER JOIN Courses C on PR.courseID = C.id
+                                WHERE C.id = $courseSelection";
+
+                        $resultPR = mysqli_query($connection, $selectPR);
+                        $count = mysqli_num_rows($resultPR);
+                        if($resultPR and $count > 0){
+                            while($rowPR = mysqli_fetch_array($resultPR)){
+
+                                $selectPName = "SELECT title, courseNumber
+                                    FROM Courses 
+                                    WHERE id = $rowPR[0]";
+                                $resultPName = mysqli_query($connection, $selectPName);
+                                $rowName = mysqli_fetch_array($resultPName);
+                                echo "<ul>
+                                        <li>$rowName[1]: $rowName[0]</li>
+                                    </ul>";
+                                
+                            }
+                        }else{
+                            echo "<ul>
+                                    <li><em>N/A</em></li>
+                                </ul>";
+                        }
+
+                    }
+                    
+                }
+                
+                
 
 
             }
